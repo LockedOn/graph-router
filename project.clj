c906a562d88b6469944dcf1b3cd56a8dbaab6daa
Karl Mikkelsen <karl@karlmikko.com>
2015-09-15 15:58:54 +1000

    Version 0.1.4

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-    			 [prismatic/schema "1.0.1"]]
+    			 [prismatic/schema "0.4.4"]]
   :deploy-repositories [["releases" :clojars]]
   :main graph-router.core)
